2bf13d020e8c7344fbbbcd26079ba88d3054bf6a
versprechen4023 <versprechen4023@naver.com>
2023-08-16 10:55:08 +0900

    삭제

--- Groom/src/main/webapp/js/my.js
+++
@@ -1,9 +0,0 @@
-var IMP = window.IMP; 
-IMP.init("imp70038768"); 
-					      
-var today = new Date();   
-var hours = today.getHours(); // 시
-var minutes = today.getMinutes();  // 분
-var seconds = today.getSeconds();  // 초
-var milliseconds = today.getMilliseconds();
-var makeMerchantUid = hours +  minutes + seconds + milliseconds;
